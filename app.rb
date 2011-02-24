e90e07f002a2945099791fefdd4d00c4e815a070
Riley Dutton <riley@dutton.us>
2011-02-24 12:42:36 -0600

    Fixed bugs, added setup_info

--- app.rb
+++ app.rb
@@ -5,10 +5,10 @@ require 'config'
 
 enable :logging
 
+client = PlayNicely::Client.new(PLAYNICELY_USERNAME, PLAYNICELY_PASSWORD)
+
 post '/feedback' do
-  
-  client = PlayNicely::Client.new(PLAYNICELY_USERNAME, PLAYNICELY_PASSWORD)
-  
+  me = client.current_user
   if(params[:type] == 'bug')
     type = 'bug'
   elsif(params[:type] == 'idea')
@@ -17,12 +17,21 @@ post '/feedback' do
     type = 'bug'
   end
   
+  if(!params[:short])
+    params[:short] = ""
+  end
+  
+  if(!params[:long])
+    params[:long] = ""
+  end
+  
   begin
   
   item = client.create_item(PLAYNICELY_PROJECT_ID, {
     :project_id => PLAYNICELY_PROJECT_ID,
-    :involved => [1426],
-    :responsible => 1426,
+    :milestone_id => PLAYNICELY_MILESTONE_ID,
+    :involved => [me.user_id],
+    :responsible => me.user_id,
     :subject => params[:short],
     :body => params[:long],
     :tags => ["feedback"],
@@ -41,4 +50,17 @@ end
 
 get '/' do
   File.read('index.html')
+end
+
+get '/setup_info' do
+  me = client.current_user
+  my_projects = client.user_projects(me.user_id)
+  puts "Logged in as #{me.username} ID: #{me.user_id}"
+  puts "Your projects are:"
+  my_projects.each do |project|
+      puts "#{project.name} (ID: #{project.project_id})"
+  end
+  
+  puts YAML::dump client.project_milestones(PLAYNICELY_PROJECT_ID)
+  "See your console log for relevant information"
 end
